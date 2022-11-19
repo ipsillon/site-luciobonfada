0f0d3cc2c5bb14903032b6165d0b86e5a2a565a0
Régis Picáz Tavares <regispicaz@pop-os.localdomain>
2022-11-19 11:08:03 -0300

    Correção de assets

--- js/script.js
+++ js/script.js
@@ -1,39 +1,4 @@
-console.log('Hello World');
-
 // var altura = window. screen. height ;
 var altura = document.getElementById('navbar-container').clientHeight;
 
 console.log(altura);
-
-
-// jQuery(window).scroll(function () {
-//   var top = jQuery(document).scrollTop();
-//   var height = 200;
-
-//   if (top > height) {
-//       //sticky-top or fixed-top
-//       jQuery('.sticky-top').addClass('bg-opacity-75');
-//   } else {
-//       jQuery('.sticky-top').removeClass('bg-opacity-75');
-//   }
-// });
-
-// 
-// $(document).ready(function(){
-//     $(window).scroll(
-//         {
-//             previousTop: 0
-//         }, 
-//         function () {
-//         var currentTop = $(window).scrollTop();
-//         if (currentTop < this.previousTop) {
-//             //$(".sidebar em").text("Up"); /* optional for demo */
-//             $(".nav").css({ opacity: 0.5 });
-//         } else {
-//             //$(".sidebar em").text("Down");
-//             $(".nav").css({ opacity: 0 });
-//         }
-//         this.previousTop = currentTop;
-//     });
-
-// });
