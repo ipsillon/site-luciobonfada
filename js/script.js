e7d323d26edac7107ed9eea1af9e22e047ed2563
Régis Picáz Tavares <regispicaz@pop-os.localdomain>
2022-11-21 07:24:12 -0300

    Adição de efeitos em Navbar da Home disparado com scroll da página | Alinhamento de cards da pagina posrtfólio

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 var altura = document.getElementById('navbar-container').clientHeight;
 
 console.log(altura);
+
+// Funcao JQuery para o menu mobile
+$(document).ready(function(){
+  $(window).bind('scroll', function(){
+    var gap = 50;
+    if ($(window).scrollTop() > gap) {
+      $('#nav-home').addClass('active-scroll');
+    }else{
+      $('#nav-home').removeClass('active-scroll');
+    }
+  });
+});
+
